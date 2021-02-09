485d2cc6ca42eadbad73934d00d903712d0fa17f
Talib <talibjsss@gmail.com>
2021-02-09 09:41:45 +0530

    help in removing hardcodes

---
+++ sampleProject/src/sampleProject/EvenOdd.java
@@ -0,0 +1,17 @@
+package sampleProject;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=5;
+		if(num%2==0)
+			System.out.println("EVEN");
+		else
+			System.out.println("ODD");
+	
+		//heli in taking inpiut from user and removde hardcoded codes..
+	}
+
+}
